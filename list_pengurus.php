060873933e13112300a8b93ac3f736a64b160d96
rayhansavero <reyhan.saveroaril@gmail.com>
2017-11-13 10:53:25 +0700

    perubahan database + tambah data dgn id pengurus menjadi huruf+angka sukses, namun update data gagal

--- list_pengurus.php
+++ list_pengurus.php
@@ -1,4 +1,5 @@
 <?php
+$con = mysqli_connect('localhost','root','','sikati');
 session_start();
 if(!isset($_SESSION['NAMA_ADMIN']))
 {
@@ -9,39 +10,59 @@ else
 ?>
 <!DOCTYPE html>
 <html>
-
-<!--PROSES TAMBAH/EDIT DATA PENGURUS-->
 <?php
-$con=mysqli_connect('localhost','root','','sikati');
+//FUNGSI UNTUK MEMBUAT ID MENJADI AUTONUMBER (HURUF+ANGKA)
+function nomor() {
+  $con = mysqli_connect('localhost','root','','sikati');
+  $query = mysqli_query($con,"select id_pengurus from list_pengurus order by id_pengurus desc limit 0,1") or die(mysql_error());
+	list ($no_temp) = mysqli_fetch_row($query);
 
-if (isset($_POST['simpan']))
-{
-  $id= $_POST['id_pengurus'];
-  $nama= $_POST['nama_pengurus'];
-  $query= mysqli_query($con,"insert into list_pengurus values ('$id','$nama')") or die(mysql_error());
+	if ($no_temp == '') {
+		$no_urut = 'P01';
+
+		} else {
+		$jum = substr($no_temp,1,3);
+		$jum++;
+		if($jum <= 9) {
+			$no_urut = 'P0' . $jum;
+		}
+    elseif ($jum <= 99) {
+			$no_urut = 'P' . $jum;
+		}
+    else {
+			die("Nomor urut melebih batas");
+		}
+	}
+		return $no_urut;
+}
+
+$nomor = nomor();
+
+//PROSES TAMBAH/EDIT DATA PENGURUS
+if (isset($_POST['simpan'])) {
+  $nama = $_POST['nama_pengurus'];
+  $query = mysqli_query($con,"insert into list_pengurus values ('$nomor','$nama')") or die(mysql_error());
   echo "
-  <script> alert ('Data Behasil di Tambahkan');
+  <script> alert ('Data Berhasil di Tambahkan');
   document.location='list_pengurus.php';
   </script>
   ";
 }
-elseif (isset($_POST['update']))
-{
-  $id= $_POST['id_pengurus'];
-  $nama= $_POST['nama_pengurus'];
-  $query= mysqli_query($con,"update list_pengurus set id_pengurus='$id',nama_pengurus='$nama' where id_pengurus='$id' ") or die(mysql_error());
+elseif (isset($_POST['update'])) {
+  $id = $_POST['id_pengurus'];
+  $nama = $_POST['nama_pengurus'];
+  $query = mysqli_query($con,"update list_pengurus set id_pengurus='$id',nama_pengurus='$nama' where id_pengurus='$id' ") or die(mysql_error());
   echo "
-  <script> alert ('Data Behasil di Update');
+  <script> alert ('Data Berhasil di Update');
   document.location='list_pengurus.php';
   </script>
   ";
 }
-elseif (isset($_POST['hapus']))
-{
-  $id= $_POST['hapus'];
-  $query= mysqli_query($con,"delete from list_pengurus where id_pengurus='$id' ") or die(mysql_error());
+elseif (isset($_POST['hapus'])) {
+  $id = $_POST['hapus'];
+  $query = mysqli_query($con,"delete from list_pengurus where id_pengurus='$id' ") or die(mysql_error());
   echo "
-  <script> alert ('Data Behasil di Hapus');
+  <script> alert ('Data Berhasil di Hapus');
   document.location='list_pengurus.php';
   </script>
   ";
@@ -206,7 +227,7 @@ elseif (isset($_POST['hapus']))
                   <div class="modal-dialog modal-sm" role="document">
                     <div class="modal-content">
                       <div class="modal-header">
-                        <h4 class="modal-title" id="pengurusLabel">Form Edit Data Pengurus</h4>
+                        <h4 class="modal-title">Form Tambah Data Pengurus</h4>
                       </div>
                       <div class="modal-body">
                         <form action="" method="POST" role="form">
@@ -216,7 +237,7 @@ elseif (isset($_POST['hapus']))
                               <i class="material-icons">fingerprint</i>
                             </span>
                             <div class="form-line">
-                              <input onkeyup="this.value=this.value.toUpperCase()" type="text" class="form-control" placeholder="NIM" name="id_pengurus">
+                              <input type="text" class="form-control" name="id_pengurus" value="<?php echo $nomor; ?>" disabled>
                             </div>
                           </div>
                         </div>
@@ -246,15 +267,15 @@ elseif (isset($_POST['hapus']))
                   <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
                     <thead>
                       <tr>
-                        <th class="text-center">NIM</th>
+                        <th class="text-center">Id</th>
                         <th class="text-center">Nama</th>
                         <th class="text-center">Opsi</th>
                       </tr>
                     </thead>
                     <tbody>
                       <?php
-                      $qu=mysqli_query($con,"select * from list_pengurus");
-                      while($has=mysqli_fetch_row($qu))
+                      $qu = mysqli_query($con,"select * from list_pengurus");
+                      while ($has = mysqli_fetch_row($qu))
                       {
                         ?>
                         <tr>
@@ -270,7 +291,7 @@ elseif (isset($_POST['hapus']))
                               <div class="modal-dialog modal-sm" role="document">
                                 <div class="modal-content">
                                   <div class="modal-header">
-                                    <h4 class="modal-title" id="pengurusLabel">Form Tambah Data Pengurus</h4>
+                                    <h4 class="modal-title">Form Edit Data Pengurus</h4>
                                   </div>
                                   <div class="modal-body">
                                     <form action="" method="POST" role="form">
@@ -280,7 +301,7 @@ elseif (isset($_POST['hapus']))
                                           <i class="material-icons">fingerprint</i>
                                         </span>
                                         <div class="form-line">
-                                          <input onkeyup="this.value=this.value.toUpperCase()" type="text" class="form-control" placeholder="NIM" name="id_pengurus" value="<?php echo $has[0]; ?>">
+                                          <input type="text" class="form-control" name="id_pengurus" value="<?php echo $has[0]; ?>" disabled>
                                         </div>
                                       </div>
                                     </div>
--- sikati.sql
+++ sikati.sql
@@ -1,187 +1,104 @@
--- phpMyAdmin SQL Dump
--- version 4.7.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 10, 2017 at 04:50 AM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     12/11/2017 22:27:36                          */
+/*==============================================================*/
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+drop table if exists LIST_ADMIN;
 
---
--- Database: `sikati`
---
+drop table if exists LIST_DATA_KEGIATAN;
 
--- --------------------------------------------------------
+drop table if exists LIST_JUDUL_KEGIATAN;
 
---
--- Table structure for table `list_admin`
---
+drop table if exists LIST_KAS_RUTIN;
 
-CREATE TABLE `list_admin` (
-  `ID_ADMIN` varchar(9) NOT NULL,
-  `NAMA_ADMIN` varchar(30) DEFAULT NULL,
-  `LEVEL` varchar(10) DEFAULT NULL,
-  `PASSWORD` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+drop table if exists LIST_PENGURUS;
 
---
--- Dumping data for table `list_admin`
---
+drop table if exists MASTER;
 
-INSERT INTO `list_admin` (`ID_ADMIN`, `NAMA_ADMIN`, `LEVEL`, `PASSWORD`) VALUES
-('e41150559', 'Rayhan Savero', 'Bendahara', 'admin');
+/*==============================================================*/
+/* Table: LIST_ADMIN                                            */
+/*==============================================================*/
+create table LIST_ADMIN
+(
+   ID_ADMIN             varchar(2) not null,
+   LEVEL                varchar(10),
+   PASSWORD             varchar(10),
+   primary key (ID_ADMIN)
+);
 
--- --------------------------------------------------------
+/*==============================================================*/
+/* Table: LIST_DATA_KEGIATAN                                    */
+/*==============================================================*/
+create table LIST_DATA_KEGIATAN
+(
+   ID_DATA              varchar(4) not null,
+   ID_JUDUL             varchar(3),
+   ID_MASTER            varchar(4),
+   URAIAN               varchar(30),
+   DEBIT_KGT            varchar(8),
+   KREDIT_KGT           varchar(8),
+   JUMLAH               varchar(8),
+   TANGGAL              date,
+   KETERANGAN           varchar(50),
+   primary key (ID_DATA)
+);
 
---
--- Table structure for table `list_data_kegiatan`
---
+/*==============================================================*/
+/* Table: LIST_JUDUL_KEGIATAN                                   */
+/*==============================================================*/
+create table LIST_JUDUL_KEGIATAN
+(
+   ID_JUDUL             varchar(3) not null,
+   JUDUL_KEGIATAN       varchar(50),
+   primary key (ID_JUDUL)
+);
 
-CREATE TABLE `list_data_kegiatan` (
-  `ID_DATA` varchar(3) NOT NULL,
-  `ID_JUDUL` varchar(3) DEFAULT NULL,
-  `ID_MASTER` varchar(3) DEFAULT NULL,
-  `URAIAN` varchar(30) DEFAULT NULL,
-  `DEBIT_KGT` float(8,0) DEFAULT NULL,
-  `KREDIT_KGT` float(8,0) DEFAULT NULL,
-  `JUMLAH` float(8,0) DEFAULT NULL,
-  `TANGGAL` date DEFAULT NULL,
-  `KETERANGAN` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*==============================================================*/
+/* Table: LIST_KAS_RUTIN                                        */
+/*==============================================================*/
+create table LIST_KAS_RUTIN
+(
+   ID_KAS               varchar(4) not null,
+   ID_PENGURUS          varchar(3),
+   ID_MASTER            varchar(4),
+   TGL                  date,
+   JUMLAH               varchar(8),
+   primary key (ID_KAS)
+);
 
--- --------------------------------------------------------
+/*==============================================================*/
+/* Table: LIST_PENGURUS                                         */
+/*==============================================================*/
+create table LIST_PENGURUS
+(
+   ID_PENGURUS          varchar(3) not null,
+   NAMA_PENGURUS        varchar(30),
+   primary key (ID_PENGURUS)
+);
 
---
--- Table structure for table `list_judul_kegiatan`
---
+/*==============================================================*/
+/* Table: MASTER                                                */
+/*==============================================================*/
+create table MASTER
+(
+   ID_MASTER            varchar(4) not null,
+   TGL                  date,
+   DEBET_MS             varchar(8),
+   KREDIT_MS            varchar(8),
+   KET_MS               varchar(50),
+   SALDO_MS             varchar(8),
+   primary key (ID_MASTER)
+);
 
-CREATE TABLE `list_judul_kegiatan` (
-  `ID_JUDUL` varchar(3) NOT NULL,
-  `JUDUL_KEGIATAN` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+alter table LIST_DATA_KEGIATAN add constraint FK_DEBET_DAN_KREDIT foreign key (ID_MASTER)
+      references MASTER (ID_MASTER) on delete restrict on update restrict;
 
--- --------------------------------------------------------
+alter table LIST_DATA_KEGIATAN add constraint FK_MEMILIKI foreign key (ID_JUDUL)
+      references LIST_JUDUL_KEGIATAN (ID_JUDUL) on delete restrict on update restrict;
 
---
--- Table structure for table `list_kas_rutin`
---
+alter table LIST_KAS_RUTIN add constraint FK_DEBET foreign key (ID_MASTER)
+      references MASTER (ID_MASTER) on delete restrict on update restrict;
 
-CREATE TABLE `list_kas_rutin` (
-  `ID_KAS` varchar(3) NOT NULL,
-  `ID_PENGURUS` varchar(9) DEFAULT NULL,
-  `ID_MASTER` varchar(3) DEFAULT NULL,
-  `TGL` date DEFAULT NULL,
-  `JUMLAH` float(8,0) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `list_pengurus`
---
-
-CREATE TABLE `list_pengurus` (
-  `ID_PENGURUS` varchar(9) NOT NULL,
-  `NAMA_PENGURUS` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `list_pengurus`
---
-
-INSERT INTO `list_pengurus` (`ID_PENGURUS`, `NAMA_PENGURUS`) VALUES
-('E41150001', 'Handoko Sujat'),
-('E41152345', 'Joko Sableng');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `master`
---
-
-CREATE TABLE `master` (
-  `ID_MASTER` varchar(3) NOT NULL,
-  `TGL` date DEFAULT NULL,
-  `DEBET_MS` float(8,0) DEFAULT NULL,
-  `KREDIT_MS` float(8,0) DEFAULT NULL,
-  `KET_MS` varchar(30) DEFAULT NULL,
-  `SALDO_MS` float(8,0) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `list_admin`
---
-ALTER TABLE `list_admin`
-  ADD PRIMARY KEY (`ID_ADMIN`);
-
---
--- Indexes for table `list_data_kegiatan`
---
-ALTER TABLE `list_data_kegiatan`
-  ADD PRIMARY KEY (`ID_DATA`),
-  ADD KEY `FK_DEBET_DAN_KREDIT` (`ID_MASTER`),
-  ADD KEY `FK_MEMILIKI` (`ID_JUDUL`);
-
---
--- Indexes for table `list_judul_kegiatan`
---
-ALTER TABLE `list_judul_kegiatan`
-  ADD PRIMARY KEY (`ID_JUDUL`);
-
---
--- Indexes for table `list_kas_rutin`
---
-ALTER TABLE `list_kas_rutin`
-  ADD PRIMARY KEY (`ID_KAS`),
-  ADD KEY `FK_DEBET` (`ID_MASTER`),
-  ADD KEY `FK_MEMBAYAR` (`ID_PENGURUS`);
-
---
--- Indexes for table `list_pengurus`
---
-ALTER TABLE `list_pengurus`
-  ADD PRIMARY KEY (`ID_PENGURUS`);
-
---
--- Indexes for table `master`
---
-ALTER TABLE `master`
-  ADD PRIMARY KEY (`ID_MASTER`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `list_data_kegiatan`
---
-ALTER TABLE `list_data_kegiatan`
-  ADD CONSTRAINT `FK_DEBET_DAN_KREDIT` FOREIGN KEY (`ID_MASTER`) REFERENCES `master` (`ID_MASTER`),
-  ADD CONSTRAINT `FK_MEMILIKI` FOREIGN KEY (`ID_JUDUL`) REFERENCES `list_judul_kegiatan` (`ID_JUDUL`);
-
---
--- Constraints for table `list_kas_rutin`
---
-ALTER TABLE `list_kas_rutin`
-  ADD CONSTRAINT `FK_DEBET` FOREIGN KEY (`ID_MASTER`) REFERENCES `master` (`ID_MASTER`),
-  ADD CONSTRAINT `FK_MEMBAYAR` FOREIGN KEY (`ID_PENGURUS`) REFERENCES `list_pengurus` (`ID_PENGURUS`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+alter table LIST_KAS_RUTIN add constraint FK_MEMBAYAR foreign key (ID_PENGURUS)
+      references LIST_PENGURUS (ID_PENGURUS) on delete restrict on update restrict;
